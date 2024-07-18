a002cf32c76697b814d53afe04ab1ea75756c9c0
Jason <jaycgreenwald@gmail.com>
2024-07-17 22:44:32 -0700

    Turned pet card into a JS template

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const template = document.querySelector("#pet-card-template")
+
+const wrapper = document.createDocumentFragment()
+
 async function start() {
   const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
   const weatherData = await weatherPromise.json()
@@ -14,11 +18,13 @@ async function petsArea() {
   const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
   const petsData = await petsPromise.json()
   petsData.forEach(pet => {    //"pet" is like the variable that holds each itteration of the array?
-    console.log(pet.name)
-  })
-  // const ourTemperature = weatherData.properties.periods[0].temperature
+    const clone = template.content.cloneNode(true)
 
-  // document.querySelector("#temperature-output").textContent = ourTemperature
+    clone.querySelector("h3").textContent = pet.name //get the h3 tag to contain the pet name
+
+    wrapper.appendChild(clone) //append each iteration to the wrapper variable
+  })
+  document.querySelector(".list-of-pets").appendChild(wrapper) // move the whole array to the html page last
 }
 
 petsArea()
